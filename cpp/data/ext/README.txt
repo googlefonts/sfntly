f7fb48afb2c2a04489f4024a42e01b7ce2281b42
arthurhsu <arthurhsu@google.com>
2011-08-23 16:52:07 +0000

    Remove unused files

--- cpp/data/ext/README.txt
+++
@@ -1,5 +0,0 @@
-The folder is used to store fonts that are from third parties. 
-
-Due to licensing issues, we can not provide the fonts within the package.
-
-Windows users can run copy_fonts.bat to get the fonts.
--- cpp/data/ext/copy_fonts.bat
+++
@@ -1,6 +0,0 @@
-copy %SYSTEMROOT%\Fonts\arial.ttf .
-copy %SYSTEMROOT%\Fonts\arialuni.ttf .
-copy %SYSTEMROOT%\Fonts\batang.ttc .
-copy %SYSTEMROOT%\Fonts\cambria.ttc .
-copy %SYSTEMROOT%\Fonts\msmincho.ttc .
-copy %SYSTEMROOT%\Fonts\mingliu.ttc .
