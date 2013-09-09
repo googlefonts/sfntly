586db7204b7738ec80175171321689e7a6c39cfb
cibu <cibu@google.com>
2013-09-09 22:20:15 +0000

    updating data

--- adv_layout/data/testdata/wiki_words_hb_closure.py
+++ adv_layout/data/testdata/wiki_words_hb_closure.py
@@ -2,13 +2,14 @@
 
 import subprocess
 import os
+import tempfile
 
 FONT_LANGS = '/tmp/font-languages.txt'
-HB_CLOSURE_BIN = '/usr/local/google/home/cibu/harfbuzz/harfbuzz-0.9.19/util/hb-ot-shape-closure'
+HB_CLOSURE_BIN = '/usr/local/google/home/cibu/behdad-opensource-staging-area.pyotlss/closure-gsub-text.py'
 WORD_PATH = '/usr/local/google/home/cibu/sfntly/adv_layout/data/testdata/wiki_words'
 OUT_DIR = '/usr/local/google/home/cibu/sfntly/adv_layout/data/testdata/wiki_words_hb_closure'
-DIR_FILTER = 'noto' # containing string; ex: windows8/gadug
-MAX = 5000
+DIR_FILTER = '' # containing string; ex: windows8/gadug
+MAX = 500
 
 
 def getLangs():
@@ -26,11 +27,10 @@ def composeInput(map, langs, count):
   lines = []
   for lang in langs:
     lines += map[lang][:count]
-  return ''.join(lines)
+  return lines
 
-def splitOutput(text, langs, count):
+def splitOutput(lines, langs, count):
   map = {}
-  lines = text.split('\n')
   i = 0
   for lang in langs:
     map[lang] = lines[i:i+count]
@@ -41,13 +41,17 @@ def writeLangOut(map, osFont, lang):
   f = open(OUT_DIR + '/' + osFont + '/' + lang, 'w') 
   for line in map[lang]:
     f.write("%s\n" % line)
+  f.close()
 
 def hbInputOutput(font, input):
-  p = subprocess.Popen([HB_CLOSURE_BIN, '--no-glyph-names', font], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-  output = p.communicate(input)[0]
-  p.stdin.close()
-  p.wait()
-  return output
+  tmpFile = tempfile.NamedTemporaryFile(delete=False)
+  tmpFile.write(''.join(input))
+  tmpFile.close()
+  
+  p = subprocess.Popen([HB_CLOSURE_BIN, font, tmpFile.name], stdout=subprocess.PIPE)
+  lines = p.communicate()[0].split('\n')
+  os.unlink(tmpFile.name)
+  return lines
 
 def fontLangMap():
   map = {}
