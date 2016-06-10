c56b85408bab232efd7e650f0994272a174e3b92
Lei Zhang <leizleiz@users.noreply.github.com>
2016-06-09 23:30:26 -0700

    Add a bounds check to ByteArray::Get().

--- cpp/src/sfntly/data/byte_array.cc
+++ cpp/src/sfntly/data/byte_array.cc
@@ -35,6 +35,8 @@ int32_t ByteArray::SetFilledLength(int32_t filled_length) {
 }
 
 int32_t ByteArray::Get(int32_t index) {
+  if (index < 0 || index >= Length())
+    return -1;
   return InternalGet(index) & 0xff;
 }
 
