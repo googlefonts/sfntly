googlefonts/sfntly

commit 1fba3b37c98301e2c01a5a3a7a87693ecdf4b4c8

Fix undefined shifts in ReadableFontData::ReadLong.
Fixes https://crbug.com/646300

cpp/src/sfntly/data/readable_font_data.cc
@@ -170,10 +170,19 @@ int64_t ReadableFontData::ReadULongLE(int32_t index) {
 }
 
 int32_t ReadableFontData::ReadLong(int32_t index) {
-  return ReadByte(index) << 24 |
-         ReadUByte(index + 1) << 16 |
-         ReadUByte(index + 2) << 8 |
-         ReadUByte(index + 3);
+  int32_t b1 = ReadByte(index);
+  if (b1 == kInvalidByte)
+    return kInvalidLong;
+  int32_t b2 = ReadUByte(index + 1);
+  if (b2 < 0)
+    return kInvalidLong;
+  int32_t b3 = ReadUByte(index + 2);
+  if (b3 < 0)
+    return kInvalidLong;
+  int32_t b4 = ReadUByte(index + 3);
+  if (b4 < 0)
+    return kInvalidLong;
+  return static_cast<uint32_t>(b1) << 24 | b2 << 16 | b3 << 8 | b4;
 }
 
 int32_t ReadableFontData::ReadFixed(int32_t index) {
@@ -181,7 +190,13 @@ int32_t ReadableFontData::ReadFixed(int32_t index) {
 }
 
 int64_t ReadableFontData::ReadDateTimeAsLong(int32_t index) {
-  return (int64_t)ReadULong(index) << 32 | ReadULong(index + 4);
+  int32_t high = ReadULong(index);
+  if (high == kInvalidUnsigned)
+    return kInvalidLongDateTime;
+  int32_t low = ReadULong(index + 4);
+  if (low == kInvalidUnsigned)
+    return kInvalidLongDateTime;
+  return (int64_t)high << 32 | low;
 }
 
 int32_t ReadableFontData::ReadFWord(int32_t index) {

cpp/src/sfntly/data/readable_font_data.h
@@ -53,7 +53,9 @@ class ReadableFontData : public FontData,
 
   static const int32_t kInvalidByte = 128;
   static const int32_t kInvalidShort = 32768;
+  static const int32_t kInvalidLong = 0xffffffff;
   static const int32_t kInvalidUnsigned = -1;
+  static const int64_t kInvalidLongDateTime = -1;
 
   static CALLER_ATTACH ReadableFontData* CreateReadableFontData(ByteVector* b);
 
@@ -122,7 +124,7 @@ class ReadableFontData : public FontData,
 
   // Read the ULONG at the given index.
   // @param index index into the font data
-  // @return the ULONG
+  // @return the ULONG; kInvalidUnsigned if outside the bounds of the font data
   // @throws IndexOutOfBoundsException if index is outside the FontData's range
   virtual int64_t ReadULong(int32_t index);
 
@@ -140,7 +142,7 @@ class ReadableFontData : public FontData,
 
   // Read the LONG at the given index.
   // @param index index into the font data
-  // @return the LONG
+  // @return the LONG; kInvalidLong if outside the bounds of the font data
   // @throws IndexOutOfBoundsException if index is outside the FontData's range
   virtual int32_t ReadLong(int32_t index);
 
@@ -152,7 +154,8 @@ class ReadableFontData : public FontData,
 
   // Read the LONGDATETIME at the given index.
   // @param index index into the font data
-  // @return the LONGDATETIME
+  // @return the LONGDATETIME; kInvalidLongDateTime if outside the bounds of the
+  // font data
   // @throws IndexOutOfBoundsException if index is outside the FontData's range
   virtual int64_t ReadDateTimeAsLong(int32_t index);
 

commit 3723ffd0fb7a5ca5ec14d98b9686493768fdaedf

Fix ReadableFontData::ReadDateTimeAsLong().
Commit 1fba3b3 caused large ULONG values to be reinterpreted as signed
32-bit integers.

cpp/src/sfntly/data/readable_font_data.cc
@@ -192,13 +192,13 @@ int32_t ReadableFontData::ReadFixed(int32_t index) {
 }
 
 int64_t ReadableFontData::ReadDateTimeAsLong(int32_t index) {
-  int32_t high = ReadULong(index);
+  int64_t high = ReadULong(index);
   if (high == kInvalidUnsigned)
     return kInvalidLongDateTime;
-  int32_t low = ReadULong(index + 4);
+  int64_t low = ReadULong(index + 4);
   if (low == kInvalidUnsigned)
     return kInvalidLongDateTime;
-  return (int64_t)high << 32 | low;
+  return high << 32 | low;
 }
 
 int32_t ReadableFontData::ReadFWord(int32_t index) {

cpp/src/test/otf_basic_editing_test.cc
@@ -57,6 +57,7 @@ bool TestOTFBasicEditing() {
       down_cast<FontHeaderTable::Builder*>(
           font_builder->GetTableBuilder(Tag::head));
   int64_t mod_date = header_builder->Modified();
+  EXPECT_EQ(3397043097, mod_date);
   header_builder->SetModified(mod_date + 1);
   FontPtr font;
   font.Attach(font_builder->Build());