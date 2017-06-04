4d39f2bac44aebadea418ace977a568b82101b40
Roland Illig <roland.illig@gmx.de>
2017-06-04 16:33:04 +0200

    Enable glyph coverage for non-BMP codepoints.

---
+++ java/src/com/google/typography/font/sfntly/data/SfStringUtils.java
@@ -0,0 +1,17 @@
+package com.google.typography.font.sfntly.data;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public final class SfStringUtils {
+
+  public static Set<Integer> getAllCodepoints(String s) {
+    Set<Integer> list = new HashSet<Integer>();
+    for (int cp, i = 0, len = s.length(); i < len; i += Character.charCount(cp)) {
+      cp = s.codePointAt(i);
+      list.add(cp);
+    }
+    return list;
+  }
+
+}
