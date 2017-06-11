21a39f58e1eeda0346af3f6e0232b23e90f56928
Roland Illig <roland.illig@gmx.de>
2017-06-11 22:49:13 +0200

    Replace enum with interface for GlyphRenumberer.

--- java/src/com/google/typography/font/tools/subsetter/GlyphRenumberer.java
+++ java/src/com/google/typography/font/tools/subsetter/GlyphRenumberer.java
@@ -26,7 +26,7 @@ import java.util.Map;
 /**
  * A utility class for applying a mapping to glyph number references within a TrueType composite
  * glyph object.
- * 
+ *
  * @author Raph Levien
  */
 public class GlyphRenumberer {
@@ -36,20 +36,16 @@ public class GlyphRenumberer {
    * this could go away. Note that offsets are capitalized according to the OpenType spec, rather
    * than the usual Java constant convention.
    */
-  private enum Offset {
-    // Offsets relative to glyph header
-    numberOfContours(0),
-    headerEnd(10),
+  // Offsets relative to glyph header
+  private interface Offset {
+    int numberOfContours = 0;
+    int SIZE = 10;
+  }
 
-    // Offsets relative to composite glyph block
-    compositeFlags(0),
-    compositeGlyphIndex(2);
-    
-    private final int offset;
-    
-    private Offset(int offset) {
-      this.offset = offset;
-    }
+  // Offsets relative to composite glyph block
+  private interface CompositeOffset {
+    int flags = 0;
+    int glyphIndex = 2;
   }
 
   /**
@@ -58,7 +54,7 @@ public class GlyphRenumberer {
    * fully parsed. We should benchmark this implementation against one written in terms of a higher
    * level interface and measure the actual performance gain - if not significant, this
    * implementation should be scrapped in favor of the higher level one.
-   * 
+   *
    * @return renumbered glyph data
    */
   public static ReadableFontData renumberGlyph(ReadableFontData glyph,
@@ -71,32 +67,32 @@ public class GlyphRenumberer {
   /**
    * Determine whether the glyph data is a composite glyph. Should always give the same answer
    * as {@code parsedGlyph instanceof GlyphTable.CompositeGlyph}, but faster.
-   * 
+   *
    * @param glyph
    * @return true if glyph is composite
    */
   private static boolean isCompositeGlyph(ReadableFontData glyph) {
-    return glyph.length() > 0 && glyph.readShort(Offset.numberOfContours.offset) < 0;
+    return glyph.length() > 0 && glyph.readShort(Offset.numberOfContours) < 0;
   }
-  
+
   private static ReadableFontData renumberCompositeGlyph(ReadableFontData glyph,
                                                          Map<Integer, Integer> mapping) {
     WritableFontData result = WritableFontData.createWritableFontData(glyph.length());
     glyph.copyTo(result);
-    
+
     int flags = CompositeGlyph.FLAG_MORE_COMPONENTS;
-    int index = Offset.headerEnd.offset;
+    int index = Offset.SIZE;
 
     while ((flags & CompositeGlyph.FLAG_MORE_COMPONENTS) != 0) {
-      flags = glyph.readUShort(index + Offset.compositeFlags.offset);
-      int oldGlyphIndex = glyph.readUShort(index + Offset.compositeGlyphIndex.offset);
+      flags = glyph.readUShort(index + CompositeOffset.flags);
+      int oldGlyphIndex = glyph.readUShort(index + CompositeOffset.glyphIndex);
       int newGlyphIndex = mapping.get(oldGlyphIndex);
-      result.writeUShort(index + Offset.compositeGlyphIndex.offset, newGlyphIndex);
+      result.writeUShort(index + CompositeOffset.glyphIndex, newGlyphIndex);
       index += compositeReferenceSize(flags);
     }
     return result;
   }
-  
+
   /**
    * Compute the size, in bytes, of a single composite reference.
    */
