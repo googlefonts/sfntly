71b36ba9b512ddae85be6f9a3f6a3e808ae3ba82
Roland Illig <roland.illig@gmx.de>
2017-11-04 15:06:49 +0100

    Add Javadoc for AbstractNode in the FontViewer

--- java/src/com/google/typography/font/tools/fontviewer/AbstractNode.java
+++ java/src/com/google/typography/font/tools/fontviewer/AbstractNode.java
@@ -1,8 +1,15 @@
 package com.google.typography.font.tools.fontviewer;
 
 import javax.swing.JComponent;
+import javax.swing.JScrollPane;
 import javax.swing.tree.DefaultMutableTreeNode;
 
+/**
+ * The tree in the left panel is built from these nodes.
+ * Most of these nodes correspond to a "table" of the font.
+ *
+ * @see FontNode
+ */
 abstract class AbstractNode extends DefaultMutableTreeNode {
 
   @Override
@@ -14,6 +21,11 @@ abstract class AbstractNode extends DefaultMutableTreeNode {
 
   abstract JComponent render();
 
+  /**
+   * Whether the component from {@link #render()} is wrapped in a {@link JScrollPane}
+   * so that it can be arbitrarily large.
+   * Otherwise it can easily be truncated on small screens.
+   */
   boolean renderInScrollPane() {
     return true;
   }
