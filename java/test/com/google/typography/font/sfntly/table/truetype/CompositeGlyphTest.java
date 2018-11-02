06bb6bb6c785c86a46ee06b4ab6b2cf408354d0a
Roland Illig <roland.illig@gmx.de>
2018-11-02 21:44:44 +0100

    Ensure that CompositeGlyph.argument1 and argument2 can be unsigned

--- java/test/com/google/typography/font/sfntly/table/truetype/CompositeGlyphTest.java
+++ java/test/com/google/typography/font/sfntly/table/truetype/CompositeGlyphTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 public class CompositeGlyphTest {
 
   @Test
-  public void singleSimpleGlyph() {
+  public void singleSimpleGlyph_signed_xy() {
     ReadableFontData data =
         TestUtils.fromHex( //
             "FFFF", // numberOfContours
@@ -26,5 +26,25 @@ public class CompositeGlyphTest {
     assertThat(glyph.flags(0)).isEqualTo(0x0003);
     assertThat(glyph.argument1(0)).isEqualTo(-11);
     assertThat(glyph.argument2(0)).isEqualTo(-9);
+  }
+
+  @Test
+  public void singleSimpleGlyph_unsigned_xy() {
+    ReadableFontData data =
+        TestUtils.fromHex( //
+            "FFFF", // numberOfContours
+            "0000 0000 0300 0400", // xMin, yMin, xMax, yMax
+            "0001 0007", // flags, glyphIndex
+            "FFF5 FFF7"); // argument1, argument2
+    CompositeGlyph glyph = new CompositeGlyph(data);
+
+    assertThat(glyph.glyphType()).isEqualTo(Glyph.GlyphType.Composite);
+    assertThat(glyph.xMin()).isEqualTo(0);
+    assertThat(glyph.yMin()).isEqualTo(0);
+    assertThat(glyph.xMax()).isEqualTo(0x0300);
+    assertThat(glyph.yMax()).isEqualTo(0x0400);
+    assertThat(glyph.flags(0)).isEqualTo(0x0001);
+    assertThat(glyph.argument1(0)).isEqualTo(0xFFF5);
+    assertThat(glyph.argument2(0)).isEqualTo(0xFFF7);
   }
 }
